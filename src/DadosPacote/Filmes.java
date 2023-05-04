b90dc68e7015864da8863c2072f16951f0ed4e7c
Ivna <ivnafeitosa113@gmail.com>
2023-05-03 23:59:39 -0300

    menus contents

--- src/DadosPacote/Filmes.java
+++ src/DadosPacote/Filmes.java
@@ -8,6 +8,12 @@ public class Filmes {
 	private int quantidade;
 	private int situaticao;
 
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id; 
+	}
 	public String getNome() {
 		return nome;
 	}
--- src/DadosPacote/Menus.java
+++ src/DadosPacote/Menus.java
@@ -1,11 +1,41 @@
 package DadosPacote;
+import java.util.Scanner;
 
 public class Menus {
 	
 	public void loginMenu() {
+		Scanner scanner = new Scanner(System.in);
+		int opcao = 0;
+		
 		System.out.println(""
 				+ "\n1 - Login"
 				+ "\n2 - Cadastro"
 				+ "\n3 - Sair");	
+		
 	}
+	public void adminMenu() {
+		
+		int opcao = 0;
+		
+		System.out.println(""
+				+ "\n1 - Buscar Por Filme"
+				+ "\n2 - Adicionar filme"
+				+ "\n3 - Excluir filme"
+				+ "\n4 - Atualizaar filme"
+				+ "\n5 - Comprar Filme"
+				+ "\n6 - Buscar usuário"
+				+ "\n7 - atualizar Usuários"
+				+ "\n8 - Remover usuátia");	
+		
+	}
+	
+	public void userMenus() {
+		System.out.println(""
+				+ "\n1 - Buscar Por Filme"
+				+ "\n2 - Comprar filme"
+				);	
+		
+	}
+	}
+	
 }
